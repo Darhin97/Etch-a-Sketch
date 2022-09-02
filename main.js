2e376f2ac494314b1517956b0878f0fdc032273c
Derrick Ankrah <darhin97@gmail.com>
2022-09-02 22:19:57 +0000

    complete etch a sketch game

---
+++ main.js
@@ -0,0 +1,126 @@
+"use strict";
+
+const DEFAULT_COLOR = "#000000";
+const DEFAULT_MODE = "color";
+const DEFAULT_SIZE = 2;
+
+let currentMode = DEFAULT_MODE;
+let currentSize = DEFAULT_SIZE;
+let currentColor = DEFAULT_COLOR;
+
+let colorEl = document.getElementById("color");
+let colorModeEl = document.getElementById("color-mode");
+let rainbowModeEl = document.getElementById("rainbow-mode");
+let clearModeEl = document.getElementById("clear");
+let eraseModeEl = document.getElementById("erase");
+let sliderEl = document.getElementById("slider");
+let sliderSizeEl = document.getElementById("slider-size");
+let gridsEl = document.getElementById("grids");
+let buttonsEl = document.querySelectorAll(".btn");
+
+// Events
+
+colorEl.addEventListener("input", function (e) {
+  setColor(e.target.value);
+  console.log(e.target.value);
+});
+
+sliderEl.addEventListener("input", function (e) {
+  let updatesize = e.target.value;
+  sliderSizeEl.textContent = `${updatesize} x ${updatesize}`;
+  setSize(updatesize);
+  reloadGrid();
+});
+
+clearModeEl.addEventListener("click", clear);
+
+for (let btn of buttonsEl) {
+  btn.addEventListener("click", function () {
+    setMode(btn.name);
+  });
+}
+
+// functions
+function setColor(newColor) {
+  currentColor = newColor;
+}
+
+function setMode(newMode) {
+  currentMode = newMode;
+  activateBtn(newMode);
+}
+
+function activateBtn(mode) {
+  for (let btn of buttonsEl) {
+    btn.classList.remove("active");
+  }
+
+  if (mode === "color") {
+    colorModeEl.classList.add("active");
+  } else if (mode === "rainbow") {
+    rainbowModeEl.classList.add("active");
+  } else if (mode === "clear") {
+    clearModeEl.classList.add("active");
+  } else if (mode === "erase") {
+    eraseModeEl.classList.add("active");
+  }
+}
+
+function displayGridSize() {
+  sliderSizeEl.textContent = sliderEl.value;
+}
+
+function reloadGrid() {
+  clear();
+  createGrid(currentSize);
+}
+
+function setSize(newSize) {
+  currentSize = newSize;
+}
+
+let mouseDown = false;
+document.body.onmousedown = () => {
+  mouseDown = true;
+};
+document.body.onmouseup = () => {
+  mouseDown = false;
+};
+
+function changeColor(e) {
+  let color = colorEl.value;
+  if (e.type === "mouseover" && !mouseDown) return;
+  if (currentMode === "rainbow") {
+    const randomR = Math.floor(Math.random() * 256);
+    const randomG = Math.floor(Math.random() * 256);
+    const randomB = Math.floor(Math.random() * 256);
+    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
+  } else if (currentMode === "color") {
+    e.target.style.backgroundColor = color;
+  } else if (currentMode === "erase") {
+    e.target.removeAttribute("style");
+  }
+}
+
+function createGrid(size) {
+  gridsEl.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  gridsEl.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+
+  for (let i = 0; i < size * size; i++) {
+    let gridElement = document.createElement("div");
+    gridElement.classList.add("grid-element");
+    gridsEl.appendChild(gridElement);
+    gridElement.addEventListener("mousedown", changeColor);
+    gridElement.addEventListener("mouseover", changeColor);
+    gridElement.addEventListener("dblclick", erased);
+  }
+}
+
+function erased(e) {
+  e.target.removeAttribute("style");
+}
+
+function clear() {
+  gridsEl.innerHTML = "";
+  sliderSizeEl.textContent = "Select Grid Size";
+}
---
+++ plan.md
@@ -0,0 +1,19 @@
+set default input-color, default mode and size
+
+1 choose mode(color, rainbow,clear , erase )
+
+if mode == color {
+pick color from color
+}
+
+if mode == rainbow {
+set random rgb (r= randon-value, g= random-value, b = random-value)
+}
+
+if mode == clear{
+clears color of selected box(color)
+}
+
+if mode == erase {
+clear all colors
+}
